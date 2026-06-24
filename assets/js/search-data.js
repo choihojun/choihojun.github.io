77b6f8798aadae7c4c13e724af158e9232cc605f
choihojun <hchoi8@huskers.unl.edu>
2026-06-24 19:38:10 +0000

    Deploying to gh-pages from @ choihojun/choihojun.github.io@ed23cd64641f65c22dc55782f2e2c119d775d5c6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,13 +44,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-experience",
-          title: "experience",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/experience/";
-          },
         },{id: "dropdown-bookshelf",
               title: "bookshelf",
               description: "",
