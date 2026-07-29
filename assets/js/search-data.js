b3425c291281682c361be8c793eb3c0c9d17c7d2
choihojun <hchoi8@huskers.unl.edu>
2026-07-29 07:47:09 +0000

    Deploying to gh-pages from @ choihojun/choihojun.github.io@a8cbb2fec82ecd79c3c73a183e897600e6e20140 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -421,9 +421,9 @@ ninja.data = [{
           description: "Autonomous RC car with lane detection and infrared obstacle avoidance",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-human-tracking-drone",
+          title: 'Human Tracking Drone',
+          description: "Vision-based person-following drone on a Parrot AR.Drone platform",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
